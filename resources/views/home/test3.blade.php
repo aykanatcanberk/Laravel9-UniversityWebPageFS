dead00f5cf4c04b14824575ab580e8dbe4be62b8
aykanatcanberk <kayra.aykanat@gmail.com>
2022-03-26 11:53:03 +0300

    1st change

---
+++ resources/views/home/test3.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a test parameters </h1>
+Id Value:{{$id}}
+<br>
+Number VAlue:{{$number}}
+<br>
+Sum parameters:{{$id+$number}}
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,10 +13,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//1-do something in route
+Route::get('/hello', function () {
+    return view('Hello world');
+});
+//2-call view in route
 
-Route::get('/', function () {
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+// 3-call controller Function
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+//4- Route controller view
+Route::get('/test',[HomeController::class,'test'])->name('test');
+
+//5-route with parameter
+Route::get('/param/{id}/{number}',[HomeController::class,'param'])->name('param');
+
+//6 -Route with post
+Route::post('/save',[HomeController::class,'save'])->name('save');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
