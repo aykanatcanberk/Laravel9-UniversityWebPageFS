57a9f0af9fd5aff656bf354bc42d4ecfdca8be3e
aykanatcanberk <kayra.aykanat@gmail.com>
2022-04-03 19:03:35 +0300

    template

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,19 +1,72 @@
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+    <!--===============================================
+    Template Design By WpFreeware Team.
+    Author URI : http://www.wpfreeware.com/
+    ====================================================-->
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield("title")</title>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/icon" href="img/wpf-favicon.png"/>
+
+    <!-- CSS
+    ================================================== -->
+    <!-- Bootstrap css file-->
+    <link href="{{asset('assets')}}/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font awesome css file-->
+    <link href="{{asset('assets')}}/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Superslide css file-->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/superslides.css">
+    <!-- Slick slider css file -->
+    <link href="{{asset('assets')}}/css/slick.css" rel="stylesheet">
+    <!-- Circle counter cdn css file -->
+    <link rel='stylesheet prefetch' href='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/css/jquery.circliful.css'>
+    <!-- smooth animate css file -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/animate.css">
+    <!-- preloader -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/queryLoader.css" type="text/css" />
+    <!-- gallery slider css -->
+    <link type="text/css" media="all" rel="stylesheet" href="{{asset('assets')}}/css/jquery.tosrus.all.css" />
+    <!-- Default Theme css file -->
+    <link id="switcher" href="{{asset('assets')}}/css/themes/default-theme.css" rel="stylesheet">
+    <!-- Main structure css file -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Google fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    @yield("head")
 </head>
+
+
+
 <body>
-<h1>Header</h1>
-@section('sidebar')
-    This is the master sidebar.
+@include("home.header")
+
+
+
 @show
 
-<div class="container">
     @yield('content')
-</div>
-<h1>Footer</h1>
+<@include("home.footer")
 @yield('foot')
 </body>
 </html>
