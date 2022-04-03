21b73a08f288f54a9e017dacf8723364c1181cd6
aykanatcanberk <kayra.aykanat@gmail.com>
2022-04-03 19:58:57 +0300

    template

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('home.index');
+    }
+    public function test()
+    {
+        return view('home.test');
+    }
+    public function param($id,$number)
+    {
+       // echo "parameter 1st is:",$id;
+        //echo "<br> parameter 2nd:",$number;
+        //echo "<br> Sum parameters:", $id+$number;
+        return view('home.test2',
+            [
+                'id' => $id,
+                 'number'=>$number
+            ]);
+
+    }
+    public function save(Request $request)
+    {
+        echo "save Function <br>";
+        echo "First Name:",$_REQUEST["fname"];
+        echo "<br> Last Name:",$_REQUEST["lname"];
+       /*return view('home.test2',
+            [
+                'fname' => $_REQUEST["fname"],
+                'lname'=>$_REQUEST["lname"]
+
+            ]);*/
+    }
+
+
+}
